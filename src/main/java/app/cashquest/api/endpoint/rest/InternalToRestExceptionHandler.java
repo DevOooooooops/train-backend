f80cc5a4e40bcecd5a54fead15fb84519cfa598e
Mahefaa <hei.mahefa@gmail.com>
2024-01-27 02:16:31 +0300

    chore: less verbose exceptions

--- src/main/java/app/cashquest/api/endpoint/rest/InternalToRestExceptionHandler.java
+++ src/main/java/app/cashquest/api/endpoint/rest/InternalToRestExceptionHandler.java
@@ -1,47 +1,103 @@
 package app.cashquest.api.endpoint.rest;
 
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+import app.cashquest.api.endpoint.rest.model.Exception;
+import app.cashquest.api.endpoint.rest.security.exception.BadRequestException;
 import app.cashquest.api.endpoint.rest.security.exception.ForbiddenException;
 import app.cashquest.api.endpoint.rest.security.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 @Slf4j
 public class InternalToRestExceptionHandler {
 
-  @ExceptionHandler(value = {Exception.class})
-  ResponseEntity<Exception> handleDefault(Exception e) {
-    log.error("Internal error", e);
-    return new ResponseEntity<>(e, INTERNAL_SERVER_ERROR);
-  }
+    @ExceptionHandler(value = {BadRequestException.class})
+    ResponseEntity<Exception> handleBadRequest(BadRequestException e) {
+        log.info("Bad request", e);
+        return new ResponseEntity<>(toRest(e, BAD_REQUEST), BAD_REQUEST);
+    }
 
-  @ExceptionHandler(
-      value = {
-        AccessDeniedException.class,
-        ForbiddenException.class,
-        AuthenticationException.class
-      })
-  ResponseEntity<Exception> handleForbidden(Exception e) {
-    /* rest.model.Exception.Type.FORBIDDEN designates both authentication and authorization errors.
-     * Hence do _not_ HttpsStatus.UNAUTHORIZED because, counter-intuitively,
-     * it's just for authentication.
-     * https://stackoverflow.com/questions/3297048/403-forbidden-vs-401-unauthorized-http-responses */
-    log.info("Forbidden", e);
+    @ExceptionHandler(value = {DataIntegrityViolationException.class})
+    ResponseEntity<Exception> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        log.info("Bad request", e);
+        return new ResponseEntity<>(toRest(e, BAD_REQUEST), BAD_REQUEST);
+    }
 
-    return new ResponseEntity<>(e, FORBIDDEN);
-  }
+    @ExceptionHandler(value = {MissingServletRequestParameterException.class})
+    ResponseEntity<Exception> handleBadRequest(MissingServletRequestParameterException e) {
+        log.info("Missing parameter", e);
+        return handleBadRequest(new BadRequestException(e.getMessage()));
+    }
 
-  @ExceptionHandler(value = {NotFoundException.class})
-  ResponseEntity<Exception> handleNotFound(NotFoundException e) {
-    log.info("Not found", e);
-    return new ResponseEntity<>(e, NOT_FOUND);
-  }
+    @ExceptionHandler(value = {HttpRequestMethodNotSupportedException.class})
+    ResponseEntity<Exception> handleBadRequest(HttpRequestMethodNotSupportedException e) {
+        log.info("Unsupported method for this endpoint", e);
+        return handleBadRequest(new BadRequestException(e.getMessage()));
+    }
+
+    @ExceptionHandler(value = {HttpMessageNotReadableException.class})
+    ResponseEntity<Exception> handleBadRequest(HttpMessageNotReadableException e) {
+        log.info("Missing required body", e);
+        return handleBadRequest(new BadRequestException(e.getMessage()));
+    }
+
+    @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
+    ResponseEntity<Exception> handleConversionFailed(MethodArgumentTypeMismatchException e) {
+        log.info("Conversion failed", e);
+        String message = e.getCause().getCause().getMessage();
+        return handleBadRequest(new BadRequestException(message));
+    }
+
+    @ExceptionHandler(value = {NotFoundException.class})
+    ResponseEntity<Exception> handleNotFound(NotFoundException e) {
+        log.info("Not found", e);
+        return new ResponseEntity<>(toRest(e, NOT_FOUND), NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {java.lang.Exception.class})
+    ResponseEntity<Exception> handleDefault(java.lang.Exception e) {
+        log.error("Internal error", e);
+        return new ResponseEntity<>(
+                toRest(e, INTERNAL_SERVER_ERROR), INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(
+            value = {
+                    AccessDeniedException.class,
+                    ForbiddenException.class,
+                    AuthenticationException.class
+            })
+    ResponseEntity<Exception> handleForbidden(java.lang.Exception e) {
+        /* rest.model.Exception.Type.FORBIDDEN designates both authentication and authorization errors.
+         * Hence do _not_ HttpsStatus.UNAUTHORIZED because, counter-intuitively,
+         * it's just for authentication.
+         * https://stackoverflow.com/questions/3297048/403-forbidden-vs-401-unauthorized-http-responses */
+        log.info("Forbidden", e);
+        var restException = new Exception();
+        restException.setType(FORBIDDEN.toString());
+        restException.setMessage(e.getMessage());
+        return new ResponseEntity<>(restException, FORBIDDEN);
+    }
+
+    private Exception toRest(java.lang.Exception e, HttpStatus status) {
+        var restException = new Exception();
+        restException.setType(status.toString());
+        restException.setMessage(e.getMessage());
+        return restException;
+    }
 }

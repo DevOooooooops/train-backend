aa11f41a0bf632941a24dd205389e070616ce520
Onitsiky <hei.onitsiky@gmail.com>
2024-01-26 19:36:06 +0300

    feat: initialize cashquest-api application

---
+++ src/main/java/app/cashquest/api/CashquestApiApplication.java
@@ -0,0 +1,13 @@
+package app.cashquest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashquestApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CashquestApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/app/cashquest/api/CashquestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.cashquest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashquestApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

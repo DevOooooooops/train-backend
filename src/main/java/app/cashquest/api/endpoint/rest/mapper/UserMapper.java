ab3a8a5f75a3cc54bb7390b249df25b05a111097
dinasoa <hei.dinasoa@gmail.com>
2024-01-27 06:29:56 +0300

    feat: compute balance

--- src/main/java/app/cashquest/api/endpoint/rest/mapper/UserMapper.java
+++ src/main/java/app/cashquest/api/endpoint/rest/mapper/UserMapper.java
@@ -4,21 +4,26 @@ import app.cashquest.api.endpoint.rest.model.CreateUser;
 import app.cashquest.api.endpoint.rest.model.CreatedUser;
 import app.cashquest.api.endpoint.rest.model.CrupdateUser;
 import app.cashquest.api.endpoint.rest.model.Income;
+import app.cashquest.api.repository.TransactionRepository;
+import app.cashquest.api.repository.model.Transaction;
 import app.cashquest.api.repository.model.User;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import static app.cashquest.api.endpoint.rest.model.TransactionType.INCOME;
 import static app.cashquest.api.service.UserService.checkLevel;
-import static java.util.UUID.randomUUID;
 
 @Component
 @AllArgsConstructor
 public class UserMapper {
   private final PasswordEncoder passwordEncoder;
   private final IncomeMapper incomeMapper;
+  private final TransactionRepository transactionRepository;
 
   public User toDomain(CreateUser user){
     return User.builder()
@@ -37,7 +42,6 @@ public class UserMapper {
 
   public User toDomain(CrupdateUser user){
     return User.builder()
-        .id(Objects.requireNonNull(user.getUser().getId()))
         .username(Objects.requireNonNull(user.getUser()).getUsername())
         .birthdate(LocalDate.from(Objects.requireNonNull(user.getUser().getBirthDate())))
         .income(incomeMapper.toDomain(Objects.requireNonNull(user.getIncome())))
@@ -46,11 +50,20 @@ public class UserMapper {
 
   public app.cashquest.api.endpoint.rest.model.User domainToRest(User user){
     app.cashquest.api.repository.model.Income income = user.getIncome();
+    AtomicInteger balance = new AtomicInteger();
+    List<Transaction> transactionList = transactionRepository.findByUserId(user.getId());
+    transactionList
+        .forEach(transaction -> {
+          if (INCOME.equals(transaction.getType())) {
+            balance.addAndGet(transaction.getAmount());
+          } else {
+            balance.addAndGet(-transaction.getAmount());
+          }
+        });
     return new app.cashquest.api.endpoint.rest.model.User()
-//        TODO: calculate balance based on income and outcome
-        .balance(income != null ? income.getAmount() : null)
-        .income(income != null ? new Income().amount(income.getAmount())
-            .earningFrequency(income.getEarningFrequency()): null)
+        .balance(balance.get())
+        .income(new Income().amount(income.getAmount())
+            .earningFrequency(income.getEarningFrequency()))
         .user(toRest(user))
         .level(checkLevel());
   }
--- src/main/java/app/cashquest/api/repository/TransactionRepository.java
+++ src/main/java/app/cashquest/api/repository/TransactionRepository.java
@@ -1,7 +1,9 @@
 package app.cashquest.api.repository;
 
 import app.cashquest.api.repository.model.Transaction;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
+  List<Transaction> findByUserId(String userId);
 }

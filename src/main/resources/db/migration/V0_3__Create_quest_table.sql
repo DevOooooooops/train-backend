19c6e6f3b02e88a6dcdf650ee53ffac726e77ee0
Onitsiky <hei.onitsiky@gmail.com>
2024-01-27 05:15:13 +0300

    chore: create quest table

---
+++ src/main/resources/db/migration/V0_3__Create_quest_table.sql
@@ -0,0 +1,8 @@
+create table "quest" (
+    id varchar constraint user_pk primary key default uuid_generate_v4(),
+    name varchar constraint name_quest_constraint unique not null ,
+    objective_description varchar not null,
+    amount_objective double precision not null default 0,
+    points integer not null default 0,
+    required_level integer not null default 0
+);

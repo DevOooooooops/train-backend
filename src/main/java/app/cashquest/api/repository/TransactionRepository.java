cae1ab8db52e36d0326b914dbe07a48ebe83edf5
dinasoa <hei.dinasoa@gmail.com>
2024-01-27 06:29:56 +0300

    feat: compute balance

--- src/main/java/app/cashquest/api/repository/TransactionRepository.java
+++ src/main/java/app/cashquest/api/repository/TransactionRepository.java
@@ -1,9 +1,11 @@
 package app.cashquest.api.repository;
 
 import app.cashquest.api.repository.model.Transaction;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
+  List<Transaction> findByUserId(String userId);
 }

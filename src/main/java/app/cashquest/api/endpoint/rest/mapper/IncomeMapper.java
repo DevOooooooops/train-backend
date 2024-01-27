26ccde17b081c8450683ad5868eb494d06ed9de3
Mahefaa <hei.mahefa@gmail.com>
2024-01-27 08:34:31 +0300

    chore: income validator

--- src/main/java/app/cashquest/api/endpoint/rest/mapper/IncomeMapper.java
+++ src/main/java/app/cashquest/api/endpoint/rest/mapper/IncomeMapper.java
@@ -1,11 +1,16 @@
 package app.cashquest.api.endpoint.rest.mapper;
 
+import app.cashquest.api.endpoint.rest.controller.validator.IncomeValidator;
 import app.cashquest.api.endpoint.rest.model.Income;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class IncomeMapper {
+  private final IncomeValidator validator;
   public app.cashquest.api.repository.model.Income toDomain(Income rest) {
+    validator.accept(rest);
     return app.cashquest.api.repository.model.Income.builder()
         .amount(rest.getAmount() != null ? rest.getAmount() : 0)
         .earningFrequency(rest.getEarningFrequency())
